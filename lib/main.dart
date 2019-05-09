5439f3145ea60eee0d546736fcf4e5e80727cf7f
pratik037 <singhalpratik037@gmail.com>
2019-05-09 08:21:48 +0530

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,57 @@
 import 'package:flutter_web/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(newtrial());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class newtrial extends StatelessWidget {
+  // const newtrial({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hello Flutter for Web',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: myfirst(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+class myfirst extends StatelessWidget {
+  // const myfirst({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text("Hello, I am hummingbird- flutter for web"),
+        centerTitle: true,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
-          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
-          // Paint" command in Visual Studio Code) to see the wireframe for each
-          // widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, World!',
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      "https://cdn-images-1.medium.com/max/1600/1*O6Pt0GpVzFARoOap80m4lw.jpeg")),
             ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Welcome to the world of amazement",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
--- web/main.dart
+++ web/main.dart
@@ -1,57 +1,10 @@
-import 'package:flutter_web/material.dart';
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import 'package:flutter_web_ui/ui.dart' as ui;
+import 'package:newwebtrial/main.dart' as app;
 
-void main() => runApp(newtrial());
-
-class newtrial extends StatelessWidget {
-  // const newtrial({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hello Flutter for Web',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: myfirst(),
-    );
-  }
-}
-
-class myfirst extends StatelessWidget {
-  // const myfirst({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello, I am hummingbird- flutter for web"),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            width: 300,
-            height: 300,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                      "https://cdn-images-1.medium.com/max/1600/1*O6Pt0GpVzFARoOap80m4lw.jpeg")),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              "Welcome to the world of amazement",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          )
-        ],
-      )),
-    );
-  }
+main() async {
+  await ui.webOnlyInitializePlatform();
+  app.main();
 }
